0e009fe6e6cba5efa06be44e9b2d6e967c6c4c02
Konstantin <rebrin24@gmail.com>
2022-02-09 21:15:23 +0500

    hw04-garbage collector

---
+++ hw04-gc/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,22 @@
+package ru.calculator;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+
+        this.value = value;
+    }
+
+    public Data() {
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
